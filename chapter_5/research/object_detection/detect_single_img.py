e5f73bbb336d0ed1277410cb8b3e71ae83884afc
wang peng <wangpeng@megvii.com>
2018-10-25 16:05:46 +0800

    add detect single img script

--- chapter_5/research/object_detection/detect_single_img.py
+++ chapter_5/research/object_detection/detect_single_img.py
@@ -76,7 +76,7 @@ with detection_graph.as_default():
     detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
     detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
     num_detections = detection_graph.get_tensor_by_name('num_detections:0')
-    for image_path in TEST_IMAGE_PATHS:
+    for image_path in TEST_ORIGIN_PATHS:
       image = Image.open(image_path)
       # the array based representation of the image will be used later in order to prepare the
       # result image with boxes and labels on it.
